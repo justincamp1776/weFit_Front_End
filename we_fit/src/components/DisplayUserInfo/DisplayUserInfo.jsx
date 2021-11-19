c6cfeed6e38e3458a1cfb7ac30671f7d54fa1949
justincamp1776 <justincamp1776@gmail.com>
2021-11-19 11:05:17 -0500

    Created updateWeight component

--- we_fit/src/components/DisplayUserInfo/DisplayUserInfo.jsx
+++ we_fit/src/components/DisplayUserInfo/DisplayUserInfo.jsx
@@ -6,8 +6,8 @@ const DisplayUserInfo = (props) => {
       {props.userInfo ? (
         <div>
           <h3>Hello, {props.userInfo.first_name}</h3>
+          <h5> Height: {props.userInfo.height} inches</h5>
           <h5> Weight: {props.userInfo.weight}lbs</h5>
-          <h5> Weight: {props.userInfo.height} inches</h5>
         </div>
       ) : (
         <h1>Hello</h1>
